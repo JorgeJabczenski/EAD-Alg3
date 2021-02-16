63298a93dee8ec5e428521d61822fc1be31ce74c
Vinicius Tikara <32783954+tikaradate@users.noreply.github.com>
2021-02-16 12:22:34 -0300

    Remoção, Busca e impressão começadas
    Necessário checar se funciona de verdade, só foi feito direto.
    Ver se o estilo está coerente com o resto do arquivo nas funções ditas

--- Hashtable/myht.c
+++ Hashtable/myht.c
@@ -37,14 +37,34 @@ h2(int k, int m)
 }
 /************************/
 
+// seguindo a risca o algoritmo na especificao
+// retornar int ou outra coisa?
+int
+busca(int n, celula *T1, celula *T2)
+{
+    pos1 = h1(n,M);
+    pos2 = h2(n,M);
+    if(T1[pos1].vazia == 1)
+    {  
+        // para consistencia, retornar -1? ou tratar como bool o retorno?
+        return 0;
+    } else if(T1[pos1].vazia == 0) {
+        return pos1;
+    } else {  // isso basta?
+        return pos2;
+    }
+    
+}
+
 
 void 
 inserir(int n, celula *T1, celula *T2)
 {
     int pos1, pos2;
 
-    pos1 = h1(n, M);~
+    pos1 = h1(n,M);
     pos2 = h2(n,M);   /* FEIO , NAO FAZER ISSO, RETIRAR DEPOIS, SÉRIO */
+                      // num tendi o q ta feio //
 
     if (T1[pos1].vazia == 1 || (T1[pos1].excluido == 1))
     {
@@ -58,15 +78,54 @@ inserir(int n, celula *T1, celula *T2)
     }
 }
 
+
 void 
 remover(int n, celula *T1, celula *T2)
 {
-    
+    int pos1, pos2;
+
+    pos1 = h1(n,M);
+    pos2 = h2(n,M);
+    // trata tabela 2 antes
+    if(T2[pos2].num == n){
+        // acho que nao precisa marcar como excluida na tabela 2?
+        // marcar com vazia?
+        T2[pos2].vazia = 1;
+    // entao checa a tabela 1
+    } else if(T1[pos1].num == n){
+        // conforme a especificao do trabalho
+        T1[pos1].excluida = 1;
+    // necessario checar se nao existe? na especificao da a entender que nao
+    } else {
+        fprintf(stderr, "Chave %d nao encontrada", n);
+    }
 }
 
-
+// impressao simples das tabelas lado a lado
 void
-imprimir
+imprimir (celula *T1, celula *T2)
+{
+    int i, n1, n2;
+    
+    printf("T1\tT2");
+    for(i = 0; i < M; i++){
+        // -1 para significar NULL?
+        if(T1[i].excluida || T1[i].vazia)
+        }
+            n1 = -1;
+        } else {
+            n1 = T1[i].num
+        }
+        // entra o caso de nao saber o que marcar na exclusao de T2
+        if(T2[i].vazia)
+        {
+            n2 = -1;
+        } else {
+            n2 = T2[i].num;
+        }
+        printf("%d\t%d", n1, n2);
+    }
+}
 
 
 int 
